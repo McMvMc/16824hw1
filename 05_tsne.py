0b3bd09254707660ae45f2e00038939b676adf1e
McMvMc <ha_luo@163.com>
2018-03-02 16:03:37 -0500

    finally done!

--- 05_tsne.py
+++ 05_tsne.py
@@ -14,6 +14,10 @@ from functools import partial
 import cv2
 import matplotlib.pyplot as plt
 import os
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+import random
+import matplotlib.patches as mpatches
 
 from eval import compute_map
 
@@ -39,6 +43,7 @@ CLASS_NAMES = [
     'train',
     'tvmonitor',
 ]
+
 
 def weight_2_grid(kernel):
 
@@ -248,6 +253,10 @@ def vgg_fn(features, labels, mode, num_classes=20):
     return tf.estimator.EstimatorSpec(
         mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
+def read_mean_rgb(model_dir):
+    mean_rgb = tf.train.NewCheckpointReader(model_dir)\
+                        .get_tensor("vgg_16/mean_rgb")
+    return mean_rgb
 
 def alex_fn(features, labels, mode, num_classes=20):
     # Write this function
@@ -307,13 +316,7 @@ def alex_fn(features, labels, mode, num_classes=20):
         kernel_initializer=tf.truncated_normal_initializer(mean=0, stddev=0.01),
         bias_initializer=tf.zeros_initializer(),
         activation=tf.nn.relu)
-    # with tf.variable_scope('pool3') as scope:
     pool3 = tf.layers.max_pooling2d(inputs=conv5, pool_size=[3, 3], strides=2)
-        # scope.reuse_variables()
-        # weights = tf.get_variable('/weights')
-        # tf.summary.image('pool3/weights', weight_2_grid(pool3))
-    # tf.summary.image('pool3/weights', pool3)
-
 
     pool3_flat = tf.reshape(pool3, [-1, 5*5*256])
 
@@ -322,13 +325,8 @@ def alex_fn(features, labels, mode, num_classes=20):
     dropout1 = tf.layers.dropout(
         inputs=dense1, rate=0.5, training=mode == tf.estimator.ModeKeys.TRAIN)
 
-    # with tf.variable_scope('dense2') as scope:
     dense2 = tf.layers.dense(inputs=dropout1, units=4096,
                         activation=tf.nn.relu)
-        # scope.reuse_variables()
-        # weights = tf.get_variable('weights')
-        # tf.summary.image('dense2/weights', weight_2_grid(weights))
-    # tf.summary.image('dense2/weights', dense2)
 
     dropout2 = tf.layers.dropout(
         inputs=dense2, rate=0.5, training=mode == tf.estimator.ModeKeys.TRAIN)
@@ -358,24 +356,8 @@ def alex_fn(features, labels, mode, num_classes=20):
     return tf.estimator.EstimatorSpec(
         mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
-def load_pascal(data_dir, split='train'):
-    """
-    Function to read images from PASCAL data folder.
-    Args:
-        data_dir (str): Path to the VOC2007 directory.
-        split (str): train/val/trainval split to use.
-    Returns:
-        images (np.ndarray): Return a np.float32 array of
-            shape (N, H, W, 3), where H, W are 224px each,
-            and each image is in RGB format.
-        labels (np.ndarray): An array of shape (N, 20) of
-            type np.int32, with 0s and 1s; 1s for classes that
-            are active in that image.
-        weights: (np.ndarray): An array of shape (N, 20) of
-            type np.int32, with 0s and 1s; 1s for classes that
-            are confidently labeled and 0s for classes that
-            are ambiguous.
-    """
+def load_pascal(data_dir, split='test'):
+
     # Write this function
     H = 256
     W = 256
@@ -386,50 +368,26 @@ def load_pascal(data_dir, split='train'):
     f_list = [x.strip('\n') for x in f_list]
     N = len(f_list)
 
-    if split != 'test':
-        N = 10
+    images = np.zeros([1000, 224, 224, 3], np.float32)
+    labels = np.zeros([1000, 20]).astype(int)
+    weights = np.ones([1000, 20]).astype(int)
 
-    images = np.zeros([int(N), 224, 224, 3], np.float32)
-    labels = np.zeros([int(N), 20]).astype(int)
-    weights = np.ones([int(N), 20]).astype(int)
-
-    if split == 'test':
-        for i in range(int(N)):
-            print(str(i) + "/" + str(N))
-            images[i, :, :, :] = Image.open(data_dir +'/JPEGImages/'+f_list[i]
-                                         +'.jpg').resize((crop_px, crop_px), Image.ANTIALIAS)
-        for c_i in range(20):
-            class_fp = data_dir + "/ImageSets/Main/" \
-                       + CLASS_NAMES[c_i] + "_" + split + ".txt"
-            with open(class_fp) as f:
-                cls_list = f.readlines()
-            cls_list = [x.split() for x in cls_list]
-
-            for im_i in range(int(N)):
-                labels[im_i, c_i] = int(int(cls_list[im_i][1]) == 1)
-                weights[im_i, c_i] = int(int(cls_list[im_i][1]) != 0)
-
-        return images, labels, weights
-
-    # read class labels
-    for c_i in range(10):
+    for i in range(int(1000)):
+        print(str(i*3) + "/" + str(N))
+        images[i, :, :, :] = Image.open(data_dir +'/JPEGImages/'+f_list[i*3]
+                                     +'.jpg').resize((crop_px, crop_px), Image.ANTIALIAS)
+    for c_i in range(20):
         class_fp = data_dir + "/ImageSets/Main/" \
                    + CLASS_NAMES[c_i] + "_" + split + ".txt"
         with open(class_fp) as f:
             cls_list = f.readlines()
         cls_list = [x.split() for x in cls_list]
 
-        for im_i in range(len(cls_list)):
-            if int(cls_list[im_i][1]) == 1:
-                print(CLASS_NAMES[c_i]+":"+cls_list[im_i][0] + "/" + str(N))
-                images[c_i, :, :, :] = Image.open(data_dir + '/JPEGImages/' + cls_list[im_i][0]
-                                        + '.jpg').resize((crop_px, crop_px), Image.ANTIALIAS)
-                labels[c_i, c_i] = int(int(cls_list[im_i][1]) == 1)
-                weights[c_i, c_i] = int(int(cls_list[im_i][1]) != 0)
-                break
+        for im_i in range(int(1000)):
+            labels[im_i, c_i] = int(int(cls_list[im_i*3][1]) == 1)
+            weights[im_i, c_i] = int(int(cls_list[im_i*3][1]) != 0)
 
     return images, labels, weights
-
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -443,227 +401,138 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-
 def _get_el(arr, i):
     try:
         return arr[i]
     except IndexError:
         return arr
 
-def read_mean_rgb(model_dir):
-    mean_rgb = tf.train.NewCheckpointReader(model_dir)\
-                        .get_tensor("vgg_16/mean_rgb")
-    return mean_rgb
-
-def activation_2_grid(activations, px, r, c):
-    image = np.zeros((px*r,px*c),dtype=np.float32)
-    max = np.amax(activations)
-    for y in range(r):
-        for x in range(c):
-            image[y*px:(y+1)*px ,x*px:(x+1)*px] = activations[:,:,x+y*c]
-
-    return image/max
-
 def main():
     VGG_DIR = "/tmp/05_nn_vgg"
     ALEX_DIR = "/tmp/05_nn_alex"
     model_path = "vgg_16.ckpt"
 
+    mean_rgb = read_mean_rgb(model_path)
+
+    color_gen = lambda: random.randint(0, 255)
+    colors= np.zeros((20,3)).astype(int)
+    for i in range(20):
+        colors[i, 0] = color_gen()
+        colors[i, 1] = color_gen()
+        colors[i, 2] = color_gen()
+
 
     args = parse_args()
     # Load training and eval data
     print("load eval data")
-    ref_data, ref_labels, ref_weights = load_pascal(
-        args.data_dir, split='trainval')
     eval_data, eval_labels, eval_weights = load_pascal(
         args.data_dir, split='test')
+    eval_data_vgg = eval_data - mean_rgb
 
-    eval_input_fn_ref = tf.estimator.inputs.numpy_input_fn(
-        x={"x": ref_data, "w": ref_weights},
-        y=ref_labels,
-        num_epochs=1,
-        shuffle=False)
-
-    vgg_classifier = tf.estimator.Estimator(
-        model_fn=partial(vgg_fn,
-                         num_classes=20),
-        model_dir=VGG_DIR)
     alex_classifier = tf.estimator.Estimator(
         model_fn=partial(alex_fn,
                          num_classes=20),
         model_dir=ALEX_DIR)
-
-    mean_rgb = read_mean_rgb(model_path)
-    eval_data_vgg = eval_data-mean_rgb
-
-    eval_input_fn_vgg = tf.estimator.inputs.numpy_input_fn(
-        x={"x": eval_data_vgg, "w": eval_weights},
-        y=eval_labels,
-        num_epochs=1,
-        shuffle=False)
+    vgg_classifier = tf.estimator.Estimator(
+        model_fn=partial(vgg_fn,
+                         num_classes=20),
+        model_dir=VGG_DIR)
 
     eval_input_fn_alex = tf.estimator.inputs.numpy_input_fn(
         x={"x": eval_data, "w": eval_weights},
         y=eval_labels,
         num_epochs=1,
         shuffle=False)
+    eval_input_fn_vgg = tf.estimator.inputs.numpy_input_fn(
+        x={"x": eval_data_vgg, "w": eval_weights},
+        y=eval_labels,
+        num_epochs=1,
+        shuffle=False)
 
     N = eval_labels.shape[0]
 
-    # VGG eval
-    ref_vgg = list(vgg_classifier.predict(input_fn=eval_input_fn_ref))
-    pred_vgg = list(vgg_classifier.predict(input_fn=eval_input_fn_vgg))
-    nn_vgg_fc = np.zeros([10]).astype(int)
-    nn_vgg_pool = np.zeros([10]).astype(int)
-    inf = float('inf')
-    for ref_i in range(10):
-        min_fc = inf
-        min_pool = inf
-        cur_ref_fc7 = ref_vgg[ref_i]['fc7']
-        cur_ref_pool = ref_vgg[ref_i]['pool5']
-        for i in range(1,N):
-            cur_fc7 = pred_vgg[i]['fc7']
-            cur_dist_fc = np.linalg.norm(cur_ref_fc7-cur_fc7)
-            if cur_dist_fc < min_fc:
-                nn_vgg_fc[ref_i] = i
-                min_fc = cur_dist_fc
-            cur_pool = pred_vgg[i]['pool5']
-            cur_dist_pool = np.linalg.norm(cur_ref_pool - cur_pool)
-            if cur_dist_pool < min_pool:
-                nn_vgg_pool[ref_i] = i
-                min_pool = cur_dist_pool
+    # ALEX eval
+    pred_alex = list(alex_classifier.predict(input_fn=eval_input_fn_alex))
+    X_alex = [sample['fc7'] for sample in pred_alex]
 
-    # w = 10
-    # h = 10
-    fc_fig = plt.figure(1)
-    fc_fig.suptitle("vgg fc+reference response", fontsize=16)
-    plt.show()
-    pool_fig = plt.figure(2)
-    pool_fig.suptitle("vgg pooling+reference response", fontsize=16)
-    plt.show()
-    nn_fig = plt.figure(3)
-    nn_fig.suptitle("vgg nearest neighbors+reference", fontsize=16)
-    plt.show()
-    columns = 8
-    rows = 3
-    for i in range(10):
+    pca_50 = PCA(n_components=50)
+    pca_result_50 = pca_50.fit_transform(X_alex)
+
+    print('ALEX variation (PCA): {}'
+          .format(np.sum(pca_50.explained_variance_ratio_)))
+
+    X_alex = TSNE(n_components=2,
+                      verbose=1, perplexity=40)\
+                        .fit_transform(pca_result_50)
+
+    # VGG eval
+    pred_vgg = list(vgg_classifier.predict(input_fn=eval_input_fn_vgg))
+    X_vgg = [sample['fc7'] for sample in pred_vgg]
+    pca_50 = PCA(n_components=50)
+    pca_result_50 = pca_50.fit_transform(X_vgg)
+
+    print('VGG variation (PCA): {}'
+          .format(np.sum(pca_50.explained_variance_ratio_)))
+
+    X_vgg_pca = TSNE(n_components=2,
+                      verbose=1, perplexity=40)\
+                        .fit_transform(pca_result_50)
+    X_vgg = TSNE(n_components=2,
+                      verbose=1, perplexity=40)\
+                        .fit_transform(X_vgg)
+
+
+    c_cls = np.array([ colors[cls.astype(bool),:].mean(axis=0) for cls in eval_labels])
+    alex_fig = plt.figure(1)
+    alex_fig.suptitle("alexnet with pca-50", fontsize=16)
+    vgg_fig = plt.figure(2)
+    vgg_fig.suptitle("vgg without pca-50", fontsize=16)
+    vgg_pca_fig = plt.figure(3)
+    vgg_pca_fig.suptitle("vgg with pca-50", fontsize=16)
+    for i in range(N):
+        i
         plt.figure(1)
-        nn_fc = nn_vgg_fc[i]
-        nn_pool = nn_vgg_pool[i]
-        fc_img = Image.fromarray(np.reshape(
-            pred_vgg[nn_fc]['fc7']/np.amax(pred_vgg[nn_fc]['fc7'])*255,
-                                            (64, 64)))
-        fc_img_ref = Image.fromarray(np.reshape(
-            ref_vgg[i]['fc7']/np.amax(ref_vgg[i]['fc7'])*255,
-                                            (64, 64)))
-        ax = fc_fig.add_subplot(rows, columns, i*2+1)
-        ax.set_title(CLASS_NAMES[i])
-        plt.imshow(fc_img)
-        ax = fc_fig.add_subplot(rows, columns, i*2+2)
-        ax.set_title("REF: " + CLASS_NAMES[i])
-        plt.imshow(fc_img_ref)
+        plt.scatter(X_alex[i,0], X_alex[i,1],
+                c=('#%02X' % int(c_cls[i,0]))
+                  +('%02X' % int(c_cls[i,1]))
+                  +('%02X' % int(c_cls[i,2])))
+        # plt.legend((a), (CLASS_NAMES[i]))
 
         plt.figure(2)
-        pool_img = activation_2_grid(pred_vgg[nn_pool]['pool5'], 7, 16, 32)
-        pool_img_ref = activation_2_grid(ref_vgg[i]['pool5'], 7, 16, 32)
-        ax = pool_fig.add_subplot(rows, columns, i*2+1)
-        ax.set_title(CLASS_NAMES[i])
-        plt.imshow(pool_img)
-        ax = pool_fig.add_subplot(rows, columns, i*2+2)
-        ax.set_title("REF: " + CLASS_NAMES[i])
-        plt.imshow(pool_img_ref)
+        plt.scatter(X_vgg[i,0], X_vgg[i,1],
+                c=('#%02X' % int(c_cls[i,0]))
+                  +('%02X' % int(c_cls[i,1]))
+                  +('%02X' % int(c_cls[i,2])))
+        # plt.legend((b), (CLASS_NAMES[i]))
 
         plt.figure(3)
-        ax = nn_fig.add_subplot(4, 10, i*2 + 1)
-        ax.set_title("FC: " + CLASS_NAMES[i])
-        plt.imshow(Image.fromarray(eval_data[nn_vgg_fc[i]].astype(np.uint8)))
-        ax = nn_fig.add_subplot(4, 10, i*2 + 2)
-        ax.set_title("REF: " + CLASS_NAMES[i])
-        plt.imshow(Image.fromarray(ref_data[i].astype(np.uint8)))
+        plt.scatter(X_vgg_pca[i, 0], X_vgg_pca[i, 1],
+                    c=('#%02X' % int(c_cls[i, 0]))
+                      + ('%02X' % int(c_cls[i, 1]))
+                      + ('%02X' % int(c_cls[i, 2])))
+        # plt.legend((c), (CLASS_NAMES[i]))
 
-        ax = nn_fig.add_subplot(4, 10, i*2 + 1 + 20)
-        ax.set_title("POOLING: " + CLASS_NAMES[i])
-        plt.imshow(Image.fromarray(eval_data[nn_vgg_pool[i]].astype(np.uint8)))
-        ax = nn_fig.add_subplot(4, 10, i*2 + 2 + 20)
-        ax.set_title("REF: " + CLASS_NAMES[i])
-        plt.imshow(Image.fromarray(ref_data[i].astype(np.uint8)))
+    # legend = []
+    # for i in range(20):
+    #     legend = np.concatenate((legend,
+    #                  [mpatches.Patch(color=('#%02X' % int(c_cls[i,0]))
+    #               +('%02X' % int(c_cls[i,1]))
+    #               +('%02X' % int(c_cls[i,2])), label=CLASS_NAMES[i])]))
+    classes = CLASS_NAMES
+    class_colours = c_cls/255
+    recs = []
+    for i in range(0, len(class_colours)):
+        recs.append(mpatches.Rectangle((0, 0), 1, 1, fc=class_colours[i]))
 
-
-    ALEX eval
-    ref_alex = list(alex_classifier.predict(input_fn=eval_input_fn_ref))
-    pred_alex = list(alex_classifier.predict(input_fn=eval_input_fn_alex))
-    nn_alex_fc = np.zeros([10]).astype(int)
-    nn_alex_pool = np.zeros([10]).astype(int)
-    inf = float('inf')
-    for ref_i in range(10):
-        min_fc = inf
-        min_pool = inf
-        cur_ref_fc7 = ref_alex[ref_i]['fc7']
-        cur_ref_pool = ref_alex[ref_i]['pool5']
-        for i in range(1,N):
-            cur_fc7 = pred_alex[i]['fc7']
-            cur_dist_fc = np.linalg.norm(cur_ref_fc7-cur_fc7)
-            if cur_dist_fc < min_fc:
-                nn_alex_fc[ref_i] = i
-                min_fc = cur_dist_fc
-            cur_pool = pred_alex[i]['pool5']
-            cur_dist_pool = np.linalg.norm(cur_ref_pool - cur_pool)
-            if cur_dist_pool < min_pool:
-                nn_alex_pool[ref_i] = i
-                min_pool = cur_dist_pool
-
-    fc_fig = plt.figure(4)
-    fc_fig.suptitle("alex fc+reference response", fontsize=16)
+    plt.figure(1)
+    plt.legend(recs, classes, loc=4)
     plt.show()
-    pool_fig = plt.figure(5)
-    pool_fig.suptitle("alex pooling+reference response", fontsize=16)
+    plt.figure(2)
+    plt.legend(recs, classes, loc=4)
     plt.show()
-    nn_fig = plt.figure(6)
-    nn_fig.suptitle("alex nearest neighbors+reference", fontsize=16)
+    plt.figure(3)
+    plt.legend(recs, classes, loc=4)
     plt.show()
-    columns = 8
-    rows = 3
-    for i in range(10):
-        plt.figure(4)
-        nn_fc = nn_alex_fc[i]
-        nn_pool = nn_alex_pool[i]
-        fc_img = Image.fromarray(np.reshape(
-            pred_alex[nn_fc]['fc7']/np.amax(pred_alex[nn_fc]['fc7'])*255,
-                                            (64, 64)))
-        fc_img_ref = Image.fromarray(np.reshape(
-            ref_alex[i]['fc7']/np.amax(ref_alex[i]['fc7'])*255,
-                                            (64, 64)))
-        ax = fc_fig.add_subplot(rows, columns, i*2+1)
-        ax.set_title(CLASS_NAMES[i])
-        plt.imshow(fc_img)
-        ax = fc_fig.add_subplot(rows, columns, i*2+2)
-        ax.set_title("REF: " + CLASS_NAMES[i])
-        plt.imshow(fc_img_ref)
-
-        plt.figure(5)
-        pool_img = activation_2_grid(pred_alex[nn_pool]['pool5'], 5, 16, 16)
-        pool_img_ref = activation_2_grid(ref_alex[i]['pool5'], 5, 16,16)
-        ax = pool_fig.add_subplot(rows, columns, i*2+1)
-        ax.set_title(CLASS_NAMES[i])
-        plt.imshow(pool_img)
-        ax = pool_fig.add_subplot(rows, columns, i*2+2)
-        ax.set_title("REF: " + CLASS_NAMES[i])
-        plt.imshow(pool_img_ref)
-
-        plt.figure(6)
-        ax = nn_fig.add_subplot(4, 10, i*2 + 1)
-        ax.set_title("FC: " + CLASS_NAMES[i])
-        plt.imshow(Image.fromarray(eval_data[nn_alex_fc[i]].astype(np.uint8)))
-        ax = nn_fig.add_subplot(4, 10, i*2 + 2)
-        ax.set_title("REF: " + CLASS_NAMES[i])
-        plt.imshow(Image.fromarray(ref_data[i].astype(np.uint8)))
-
-        ax = nn_fig.add_subplot(4, 10, i*2 + 1 + 20)
-        ax.set_title("POOLING: " + CLASS_NAMES[i])
-        plt.imshow(Image.fromarray(eval_data[nn_alex_pool[i]].astype(np.uint8)))
-        ax = nn_fig.add_subplot(4, 10, i*2 + 2 + 20)
-        ax.set_title("REF: " + CLASS_NAMES[i])
-        plt.imshow(Image.fromarray(ref_data[i].astype(np.uint8)))
 
     return
 if __name__ == "__main__":
